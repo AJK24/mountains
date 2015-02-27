d128a5d57fe72434683e5e9e11733cc035565ebe
Andrew <akv9509@saintjoe.edu>
2015-02-27 11:22:07 -0500

    Adapted file of mountains.js

---
+++ shoes.js
@@ -0,0 +1,12 @@
+SHOES = [
+  {name: "Nike", size: 13, spokesman: "Jordan"},
+  {name: "Adidas", size: 9, spokesman: "John"},
+  {name: "Asics", size: 12, spokesman: "Keith"},
+  {name: "Prince", size: 8, spokesman: "Rafeal"},
+  {name: "New Balance", size: 6, spokesman: "Roger"},
+  {name: "Swiss", size: 10, spokesman: "Joe"},
+  {name: "Reebok", size: 11, spokesman: "Damien"}
+];
+
+if (typeof module != "undefined" && module.exports)
+	module.exports(SHOES);
